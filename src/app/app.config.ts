afe177d4d96f8787b83747461472faf6d84d6a09
Sandy <90114858+Sandy4521@users.noreply.github.com>
2024-05-13 13:00:48 +0200

    first_c

---
+++ src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { httpInterceptor } from './core/interceptors/http.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([httpInterceptor])),
+    importProvidersFrom(BrowserAnimationsModule),provideAnimationsAsync(),
+  ],
+};
